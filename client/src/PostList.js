fc96b21b82bc66301b218944154ff538bebdb4ae
Patrick <19709369+ocelot-engineering@users.noreply.github.com>
2023-04-18 19:45:44 +0000

    Add components for comments - add comment create component - add comment list component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 const PostList = () => {
     const [posts, setPosts] = useState({});
@@ -23,6 +25,8 @@ const PostList = () => {
             >
                 <div className='card-body'>
                     <h3>{post.title}</h3>
+                    <CommentCreate postId={post.id} />
+                    <CommentList postId={post.id} />
                 </div>
             </div>
         );
